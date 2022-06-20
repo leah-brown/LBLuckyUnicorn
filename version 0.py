7ad692132b6e5d5089434cd2b4d16bf48bc085ed
leah-brown <107890801+leah-brown@users.noreply.github.com>
2022-06-21 09:25:41 +1200

    skeleton of the base code

---
+++ version 0.py
@@ -0,0 +1,17 @@
+"""
+Base component for the game Lucky Unicorn
+V0 - skeleton setup
+
+author- Leah Brown
+CC LB 2022
+
+"""
+
+# import libraries below this line ************************
+
+
+# all functions below this line ***************************
+
+
+# main routine below this line ****************************
+print("Hello")
